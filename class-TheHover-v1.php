04808ecf5d147005cba0ec4b20adbfa27efe31a1
zgbjgg <zgbjgg@gmail.com>
2015-09-15 16:59:14 -0500

    return JSON response as an array instead of simple var

--- class-TheHover-v1.php
+++ class-TheHover-v1.php
@@ -74,7 +74,7 @@ class TheHover {
 		$response = curl_exec($ch);
 	
 		if ( $this->encoding == 'json') {
-			$json = json_decode($response); 
+			$json = json_decode($response, true); 
 			if (json_last_error() === JSON_ERROR_NONE) { 
    				return $json;	
 			} else {
